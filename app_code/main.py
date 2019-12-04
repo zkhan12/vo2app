8fcb09472d26da781432456750f77cc15ca76950
zkhan12 <zoheb1112@gmail.com>
2019-12-03 20:38:42 -0600

    added parser for app

--- app_code/main.py
+++ app_code/main.py
@@ -1,8 +1,46 @@
-import process
+from process import process_csv
+from argparse import ArgumentParser
+from argparse import RawTextHelpFormatter
+
+
+def _get_parser():
+    """
+    Build the parser for command line utilities
+
+    :returns parser:contains options for command line
+    """
+
+    parser = ArgumentParser(description='vo2 percentile arguments',
+                            formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument('input', help='The path of the input csv with '
+                        'sex, age, and vo2 columns')
+
+    parser.add_argument('-o', '-output', default='none',
+                        help='Optional path of output csv')
+
+    parser.add_argument('-s', '-source', default='KUMC',
+                        choices=['KUMC', 'shiny-data'],
+                        help='Data source to get percentiles from, default is KUMC')
+
+    return parser
 
 
 def main():
-    process.process_csv('sample.csv')
+    """
+    Filter command line options
+    """
+
+    opts = _get_parser().parse_args()  # get options from command line
+
+    if opts.input[-4:] != '.csv':
+        raise IOError('Input file is not a csv')
+
+    if opts.o != 'none':
+        if opts.o[-4:] != '.csv':
+            raise IOError('Output file is not a csv')
+
+    process_csv(opts.input, opts.o, opts.s)
 
 
 if __name__ == '__main__':
--- app_code/process.py
+++ app_code/process.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import fit_equation
+from fit_equation import get_percentile
+from datetime import datetime
+from os import path
 
 _default_cols = ['sex', 'age', 'vomax']
 
@@ -12,7 +14,7 @@ _male_row_values = ['m', 'male', 'MALE', 'Male', 'M']
 _female_row_values = ['f', 'female', 'FEMALE', 'Female', 'F']
 
 
-def process_csv(path, source='fit-data'):
+def process_csv(path, output, source):
     """
     Reads in .csv file and processes it before adding percentiles
 
@@ -36,7 +38,10 @@ def process_csv(path, source='fit-data'):
 
     df = _get_percentiles(df, source)
 
-    df.to_csv('output/out.csv', index=False)
+    if output == 'none':
+        df.to_csv(_get_file_name(), index=False)
+    else:
+        df.to_csv(output, index=False)
 
 
 def _get_percentiles(df, source):
@@ -48,14 +53,15 @@ def _get_percentiles(df, source):
     :returns df:dataframe with percentile values added
     """
 
-    if source == 'fit-data':
+    if (source == 'fit-data') or (source == 'KUMC'):  # TODO fix this once names are established
         for index, row in df.iterrows():
-            percentile = fit_equation.get_percentile(row['sex'], row['age'], row['vomax'])
+            percentile = get_percentile(row['sex'], row['age'], row['vomax'])
             df.loc[index, 'percentile'] = percentile
 
     elif source == 'shiny-data':
-        pass
         # TODO
+        print('This source integration is still in progress')
+        quit()
 
     return df
 
@@ -101,3 +107,18 @@ def _standardize_cols(df):
     formatted_df = df[_default_cols]
 
     return formatted_df
+
+
+def _get_file_name():
+    """
+    Generate the output file name and place it in output directory.
+    Note: there is probably a better way to do this using relative file paths
+          but I couldn't get it working with out setting cwd
+
+    :returns file_path:the path of the output file
+    """
+
+    file_name = str(datetime.date(datetime.today())) + '.csv'
+    file_path = path.abspath(path.join(path.dirname(__file__), '..', 'output', file_name))
+
+    return file_path
